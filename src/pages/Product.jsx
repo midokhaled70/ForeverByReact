2bdb1a0475b7c9c26925f380ca720f4a818c4450
midokhaled70 <ahmedkhaled172001@icloud.com>
2025-04-15 22:25:04 +0200

    Updated feature X or fixed issue Y

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -6,15 +6,23 @@ const Product = () => {
   const {ProductId}=useParams();
   const {products}=useContext(ShopContext);
   const [productData,setProductData]=useState(false);
-
+const [image,setImage]=useState('')
   const fetchProductData=async()=>{
-
+products.map((item)=>{
+if(item._id===ProductId){
+  setProductData(item)
+setImage(item.image[0])
+  return null;
+}
+})
   }
   useEffect(()=>{
+    fetchProductData();
+  },[ProductId,products])
+  return productData   (
+    <div>
 
-  },[])
-  return (
-    <div>Product</div>
+    </div>
   )
 }
 
