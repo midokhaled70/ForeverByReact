e4a6796f9f3596261c34949d6de28af46f05df71
midokhaled70 <ahmedkhaled172001@icloud.com>
2025-04-16 10:54:05 +0200

    Updated feature X or fixed issue Y

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -19,11 +19,25 @@ setImage(item.image[0])
   useEffect(()=>{
     fetchProductData();
   },[ProductId,products])
-  return productData   (
-    <div>
+  return productData ?   (
+    <div className='border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100'>
+<div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>
 
+<div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
+<div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
+{
+  productData.image.map((item,index)=>(
+    <img onClick={()=>setImage(item)} src={item}key={index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' alt='' />
+  ))
+}
+</div>
+<div className='w-full sm:w-[88%]'>
+  <img  className='w-full h-auto ' src={image} alt=''/>
+</div>
+</div>
+</div>
     </div>
-  )
+  ): <div className='opacity-0'></div>
 }
 
 export default Product
