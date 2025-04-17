63704dcdd9d865f3d9f77b1a6a1b87040d71f752
midokhaled70 <ahmedkhaled172001@icloud.com>
2025-04-17 11:21:29 +0200

    Updated feature X or fixed issue 17-4Y

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,12 +1,14 @@
 import React, { use, useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
+import assets from '../assets/frontend_assets/assets';
 
 const Product = () => {
   const {ProductId}=useParams();
-  const {products}=useContext(ShopContext);
+  const {products,currency}=useContext(ShopContext);
   const [productData,setProductData]=useState(false);
 const [image,setImage]=useState('')
+const [size,setSize]=useState( '')
   const fetchProductData=async()=>{
 products.map((item)=>{
 if(item._id===ProductId){
@@ -35,6 +37,33 @@ setImage(item.image[0])
   <img  className='w-full h-auto ' src={image} alt=''/>
 </div>
 </div>
+
+<div className='flex-1'>
+<h1 className='font-medium text-2xl mt-2'>{productData.name}</h1>
+<div className='flex items-center gap-1 mt-2'>
+<img src={assets.star_icon}  className="w-3 5"alt=''/>
+<img src={assets.star_icon}  className="w-3 5"alt=''/>
+<img src={assets.star_icon}  className="w-3 5"alt=''/>
+<img src={assets.star_icon}  className="w-3 5"alt=''/>
+<img src={assets.star_dull_icon}  className="w-3 5"alt=''/>
+<p className='p1-2'>(122)</p>
+
+</div>
+<p className='mt-5 text-3xl font-medium'>{currency}{productData.price}</p>
+<p className='mt-5 text-gray-500 md:w-4/5'>{productData.description}</p>
+
+<div className='flex flex-col gap-4 my-8'>
+<p>Select Size</p>
+<div className='flex-gap-2'>
+{productData.sizes.map((item,index)=>(
+  <button className={`border py-2 px-4 bg-gray-100 `} key={index}>{item}</button>
+))}
+</div>
+</div>
+</div>
+
+
+
 </div>
     </div>
   ): <div className='opacity-0'></div>
