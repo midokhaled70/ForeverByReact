9a50bac98cf7601bb7e432f91066fb201e71c9d5
midokhaled70 <ahmedkhaled172001@icloud.com>
2025-06-03 11:57:29 +0300

    Updated feature X or fixed issue Y

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,39 +1,64 @@
 import React, { createContext, useEffect, useState } from 'react';
 import { products } from '../assets/frontend_assets/assets';
+import { toast } from 'react-toastify';
 
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
   const currency = '$';
   const delivery_fee = 10;
-const [search,setSearch]=useState('');
-const [showSearch,setShowSearch]=useState(false);
-const [CartItems,setCartItems]=useState({});
+  const [search, setSearch] = useState('');
+  const [showSearch, setShowSearch] = useState(false);
+  const [CartItems, setCartItems] = useState({});
 
-const addToCart=async (itemId,size)=>{
-let cartData=structuredClone(CartItems)
-if(cartData[itemId]){
-  if(cartData[itemId][size]){
-cartData[itemId][size]+=1
-  }
-  else{
-    cartData[itemId][size]=1;
-  }
-}
-else{
-  cartData[itemId]={};
-  cartData[itemId][size]=1
-}
-setCartItems(cartData);
-}
-useEffect(()=>{
-console.log(CartItems)
-},[CartItems])
+  const addToCart = async (itemId, size) => {
+    if (!size) {
+      toast.error('Select Product Size');
+      return;
+    }
+    let cartData = structuredClone(CartItems);
+    if (cartData[itemId]) {
+      if (cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+      } else {
+        cartData[itemId][size] = 1;
+      }
+    } else {
+      cartData[itemId] = {};
+      cartData[itemId][size] = 1;
+    }
+    setCartItems(cartData);
+  };
+
+  useEffect(() => {
+    console.log(CartItems);
+  }, [CartItems]);
+
+  const getCartCount = () => {
+    let totalCount = 0;
+    for (const itemId in CartItems) {
+      const sizes = CartItems[itemId];
+      for (const size in sizes) {
+        const quantity = sizes[size];
+        if (quantity > 0) {
+          totalCount += quantity;
+        }
+      }
+    }
+    return totalCount;
+  };
+
   const value = {
     products,
     currency,
-    delivery_fee,search,setSearch,showSearch,setShowSearch
-    ,addToCart,CartItems
+    delivery_fee,
+    search,
+    setSearch,
+    showSearch,
+    setShowSearch,
+    addToCart,
+    CartItems,
+    getCartCount,  // <-- expose here
   };
 
   return (
